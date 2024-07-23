d2128f9ea96206ab94cfc86696550994a6978311
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-07-23 15:57:27 +1200

    Added Job title input box and changed the functionality of the program to use it.

--- src/App.tsx
+++ src/App.tsx
@@ -8,123 +8,211 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro",
-  systemInstruction: {
-    role: "Interviewer",
-    parts: [
-      { text: "Job role will be entered in the first user input" },
-      { text:
-          "ignore the greetings. if the first input is not a valid job role," +
-          " send a message to the user and say 'Ending interview. Try again with a valid job title.'."},
-      {
-        text: "start by welcoming to the interview and asking the user's name and his background.",
-      },
-      { text: "ask 2 questions. one question at a time." },
-      { text: "don't mark the question like 'question 1' etc." },
-      {
-        text: "if the user doesn't answer the questions accordingly, ask the question again.",
-      },
-      {
-        text: "provide feedback about quality of the answers and where user can improve himself on.",
-      },
-      { text: "end with 'Best of luck' and the name of the user." },
-    ],
-  },
+    model: "gemini-1.5-pro",
+    systemInstruction: {
+        role: "Interviewer",
+        parts: [
+            { text: "Job role will be entered in the first user input" },
+            {
+                text:
+                    "ignore the greetings. if the first input is not a valid job role," +
+                    " send a message to the user and say 'Ending interview. Try again with a valid job title.'.",
+            },
+            {
+                text: "start by welcoming to the interview and asking the user's name and his background.",
+            },
+            { text: "ask 2 questions. one question at a time." },
+            { text: "don't mark the question like 'question 1' etc." },
+            {
+                text: "if the user doesn't answer the questions accordingly, ask the question again.",
+            },
+            {
+                text: "provide feedback about quality of the answers and where user can improve himself on.",
+            },
+            { text: "end with 'Best of luck' and the name of the user." },
+        ],
+    },
 });
 
 const chat = model.startChat({
-  history: [], // Start with an empty history
-  generationConfig: {
-    maxOutputTokens: 500,
-  },
+    history: [], // Start with an empty history
+    generationConfig: {
+        maxOutputTokens: 500,
+    },
 });
 
 function App() {
-  type Message = {
-    text: string;
-    user: boolean;
-  };
+    type Message = {
+        text: string;
+        user: boolean;
+    };
 
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [input, setInput] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [input, setInput] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const [startInterview, setStartInterview] = useState<boolean>(false);
+    const [jobTitle, setJobTitle] = useState<string>("");
 
-  const containerRef = useRef<HTMLDivElement | null>(null);
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const JobTitleRef = useRef<HTMLInputElement | null>(null);
 
-  useEffect(() => {
-    const element = containerRef.current;
-    if (element) {
-      element.scrollTop = element.scrollHeight;
-    }
-  }, [input]);
+    useEffect(() => {
+        JobTitleRef.current?.focus();
+    }, []);
 
-  const handleSendMessage = async () => {
-    if (input.trim()) {
-      const newMessages = [...messages, { text: input, user: true }];
-      setMessages(newMessages);
-      setInput("");
-      try {
+    useEffect(() => {
+        console.log(messages);
+    }, [messages]);
+
+    useEffect(() => {
+        const element = containerRef.current;
+        if (element) {
+            element.scrollTop = element.scrollHeight;
+        }
+    }, [input]);
+
+    async function beginInterview(): Promise<void> {
+        if (!jobTitle.trim()) {
+            JobTitleRef.current?.focus();
+            return;
+        }
+        setMessages([]);
+        setStartInterview(true);
         setLoading(true);
-        const result = await chat.sendMessage(input);
-        const response = result.response;
-        const text = response.text();
-        setLoading(false);
-        setMessages([...newMessages, { text: text, user: false }]);
-      } catch (error) {
-        console.error("Error sending message:", error);
-        setLoading(false);
-        setMessages([
-          ...newMessages,
-          { text: "Error: Could not get response from AI", user: false },
-        ]);
-      }
-    }
-  };
+        const AIResponse = await getAIResponse(jobTitle);
 
-  return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-blue-900">
-      <h1 className="mb-8 font-bold text-[2rem] text-blue-50 [text-shadow:_3px_3px_0_rgb(0_0_255_/_40%)]">
-        Gemini AI - Mock Interviewer
-      </h1>
-      <div className="bg-white w-full max-w-lg rounded-lg overflow-hidden shadow-gray-700 shadow-lg">
-        <div className="mt-1 p-4 h-96 overflow-y-scroll" ref={containerRef}>
-          {messages.map((msg, index) => (
-            <div
-              key={index}
-              className={`flex ${
-                msg.user ? "justify-end" : "justify-start"
-              } mb-2`}
-            >
-              <div
-                className={`rounded-lg p-2 shadow-md overflow-x-hidden flex flex-wrap ${
-                  msg.user ? "bg-blue-500 text-white" : "bg-gray-200"
-                }`}
-              >
-                <ReactMarkdown>{msg.text}</ReactMarkdown>
-              </div>
+        setMessages((messages) => [
+            ...messages,
+            { text: AIResponse, user: false },
+        ]);
+        setLoading(false);
+    }
+
+    const handleSendMessage = async () => {
+        if (input.trim()) {
+            // Add the user's message to the state
+            setMessages((messages) => [
+                ...messages,
+                { text: input, user: true },
+            ]);
+
+            // Clear the input field and set loading to true
+            setInput("");
+            setLoading(true);
+
+            // Get the AI's response
+            const AIResponse = await getAIResponse(input);
+
+            // Add the AI's message to the state using the latest state
+            setMessages((messages) => [
+                ...messages,
+                { text: AIResponse, user: false },
+            ]);
+
+            // Set loading to false
+            setLoading(false);
+            if (
+                AIResponse.includes("Ending interview") ||
+                AIResponse.includes("Best of luck")
+            ) {
+                setStartInterview(false);
+                JobTitleRef.current!.value = "";
+                setJobTitle("");
+                JobTitleRef.current?.focus();
+            }
+        }
+    };
+
+    async function getAIResponse(chatInput: string): Promise<string> {
+        try {
+            const result = await chat.sendMessage(chatInput);
+            const response = result.response;
+            const text = response.text();
+            return text;
+        } catch (error) {
+            console.error("Error sending message:", error);
+            return "Error";
+        }
+    }
+
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-blue-900">
+            <h1 className="mb-5 font-bold text-[2rem] text-blue-50 [text-shadow:_3px_3px_0_rgb(0_0_0_/_20%)]">
+                Gemini AI - Mock Interviewer
+            </h1>
+            <div className="flex mb-3 p-4 bg-white rounded-lg w-[512px] gap-2 shadow-gray-700 shadow-md items-center">
+                <h2 className="font-bold text-gray-600">Job Title: </h2>
+                <input
+                    className="bg-gray-100 border border-gray-300 rounded-md w-[400px] p-1
+          focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent"
+                    type="text"
+                    placeholder="Enter job title"
+                    onChange={(e) => setJobTitle(e.target.value)}
+                    ref={JobTitleRef}
+                    onKeyDown={(e) => e.key === "Enter" && beginInterview()}
+                />
             </div>
-          ))}
-          {loading && <div className={"loader"}></div>}
+
+            <div className="bg-white w-full max-w-lg rounded-lg overflow-hidden shadow-gray-700 shadow-lg">
+                <div
+                    className="mt-1 p-4 h-96 overflow-y-scroll"
+                    ref={containerRef}
+                >
+                    {messages.map((msg, index) => (
+                        <div
+                            key={index}
+                            className={`flex ${
+                                msg.user ? "justify-end" : "justify-start"
+                            } mb-2`}
+                        >
+                            <div
+                                className={`rounded-lg p-2 shadow-md overflow-x-hidden flex flex-wrap ${
+                                    msg.user
+                                        ? "bg-blue-500 text-white"
+                                        : "bg-gray-200"
+                                }`}
+                            >
+                                <ReactMarkdown>{msg.text}</ReactMarkdown>
+                            </div>
+                        </div>
+                    ))}
+                    {loading && <div className={"loader"}></div>}
+                </div>
+                {!startInterview ? (
+                    <div className="flex justify-center p-4 border-t border-gray-200 flex">
+                        <button
+                            className="p-2 border border-blue-500 w-[200px] rounded-lg bg-blue-500 text-white font-bold shadow-sm shadow-gray-700
+        active:bg-blue-300 active:border-blue-300 active:shadow-gray-100 active:shadow-md active:text-blue-700
+        hover:bg-blue-700"
+                            onClick={beginInterview}
+                        >
+                            Start Interview
+                        </button>
+                    </div>
+                ) : (
+                    <div className="p-4 border-t border-gray-200 flex">
+                        <input
+                            type="text"
+                            className="flex-1 p-2 border border-gray-300 rounded-lg outline-none"
+                            placeholder="Type your message..."
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            onKeyDown={(e) =>
+                                e.key === "Enter" && handleSendMessage()
+                            }
+                            autoFocus
+                        />
+                        <button
+                            className="ml-2 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition-all"
+                            onClick={handleSendMessage}
+                        >
+                            <FaPaperPlane />
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
-        <div className="p-4 border-t border-gray-200 flex">
-          <input
-            type="text"
-            className="flex-1 p-2 border border-gray-300 rounded-lg outline-none"
-            placeholder="Type your message..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
-          />
-          <button
-            className="ml-2 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition-all"
-            onClick={handleSendMessage}
-          >
-            <FaPaperPlane />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
